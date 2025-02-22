3438371e646e6d6819744a3acaef6e117ce24e82
KamilHuseynov255 <Gusey_fy78@itstep.edu.az>
2025-02-22 21:42:21 +0400

    Add First endpoint

---
+++ HelpWithAll/Models/Helper.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpWithAll.Models
+{
+    public class Helper
+    {
+        public string? Name{ get; set; }
+        public string? Surname{ get; set; }
+        public float PaymentPerHour{ get; set; }
+    }
+}
